70ddbff398829baca07b9ffae362c3aaeae596e5
donghaiy <donghaiy@jp.ibm.com>
2020-02-08 22:40:18 +0900

    update

---
+++ src/test/java/chapters/introduction/MyApp1.java
@@ -0,0 +1,20 @@
+package chapters.introduction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class MyApp1 {
+
+	final static Logger logger =  LoggerFactory.getLogger(MyApp1.class);
+	
+	  public static void main(String[] args) {
+
+		  logger.info("Entering application.");
+		  Foo foo = new Foo();
+		  foo.doIt();
+		  logger.info("Exiting application.");
+	  }
+
+
+}
